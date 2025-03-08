995d6855db02fe67ce1dc55db33574b9f596aeaf
Priyansh Agarwal <140069604+priyansh201480@users.noreply.github.com>
2025-03-08 21:14:10 +0530

    Update FavouriteButton.jsx

--- src/components/FavouriteButton.jsx
+++ src/components/FavouriteButton.jsx
@@ -1,17 +1,32 @@
-import { useContext } from "react";
-import FavouriteContext from "../context/FavouriteContext"; //
+import { useEffect, useState } from "react";
 import "../components/FavouriteButton.css";
 
 const FavouriteButton = ({ movie }) => {
-  const { favourites, addFavourite, removeFavourite } = useContext(FavouriteContext);
+  const [isFavourite, setIsFavourite] = useState(false);
 
-  const isFavourite = favourites.some((fav) => fav.imdbID === movie.imdbID);
+  useEffect(() => {
+    const savedFavourites = JSON.parse(localStorage.getItem("favourites")) || [];
+    setIsFavourite(savedFavourites.some((fav) => fav.imdbID === movie.imdbID));
+  }, [movie]);
+
+  const toggleFavourite = () => {
+    const savedFavourites = JSON.parse(localStorage.getItem("favourites")) || [];
+
+    if (isFavourite) {
+      // Remove from favourites
+      const updatedFavourites = savedFavourites.filter((fav) => fav.imdbID !== movie.imdbID);
+      localStorage.setItem("favourites", JSON.stringify(updatedFavourites));
+      setIsFavourite(false);
+    } else {
+      // Add to favourites
+      const updatedFavourites = [...savedFavourites, movie];
+      localStorage.setItem("favourites", JSON.stringify(updatedFavourites));
+      setIsFavourite(true);
+    }
+  };
 
   return (
-    
-    <button
-      className={`favourite-button ${isFavourite ? "remove" : ""}`}
-      onClick={() => isFavourite ? removeFavourite(movie.imdbID) : addFavourite(movie)}>
+    <button className={`favourite-button ${isFavourite ? "remove" : ""}`} onClick={toggleFavourite}>
       {isFavourite ? "Remove from Favourites" : "Add to Favourites"}
     </button>
   );
